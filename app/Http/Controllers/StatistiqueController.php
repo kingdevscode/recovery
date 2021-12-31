0ef30e66a71d454c303e2be696e3c9a337ddd0df
corine21 <corinedongmo2002@gmail.com>
2021-12-31 13:49:51 +0100

    recuperation du travil de samatha su rles migration

---
+++ app/Http/Controllers/StatistiqueController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Categorie;
+use App\Models\Perso;
+use App\Models\User;
+use App\Models\User;
+use App\Models\User;
+use App\Models\User;
+use App\Models\User;
+use App\Models\User;
+use App\Models\User;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class StatistiqueController extends Controller
+{
+   public function index(){
+
+   }
+}
